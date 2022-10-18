e0d18ea7c5eb1303fbb55ce9e1aaf8e6eeb31b46
Minosh_Imantha <86770967+minosh00@users.noreply.github.com>
2022-10-18 17:22:35 +0530

    push

---
+++ backend/test/userController.test.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const adminData = { Fullname: 'minosh imantha ', email: 'minoshadmin@gmail.com', pNumber: '0715681831', password: 'Admin', date:"2022-12-23", userRole:"admin" };
+describe('Admin Model Test', () => {
+
+
+    beforeAll(async () => {
+        process.env.DB_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        };
+    });
+
+    it('create admin without required field should failed', async () => {
+        const adminWithoutRequiredField = new User({ Fullname: 'minoshadmin' });
+        let err;
+        try {
+            const savedAdminWithoutRequiredField = await adminWithoutRequiredField.save();
+            error = savedAdminWithoutRequiredField;
+        } catch (error) {
+            err = error
+        }
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+        expect(err.errors.password).toBeDefined();
+    });
+
+})
+
